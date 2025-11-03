f108886f6229d96512ad7502b4134cdb42e288e1
Nitin <nitinkmr.4169@gmail.com>
2025-11-03 10:02:46 +0530

    changed the cred line in firebase_config.py

--- db/firebase_config.py
+++ db/firebase_config.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate("config/firebase_key.json")
+
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
