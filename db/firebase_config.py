58a3fb2de5c44c548b80a2b9a480e62a2b9554ce
Nitin <nitinkmr.4169@gmail.com>
2025-11-03 10:12:52 +0530

    again i changed the code for the firebase_config.py

--- db/firebase_config.py
+++ db/firebase_config.py
@@ -1,8 +1,13 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("config/firebase_key.json")
+# Automatically build the correct path
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # goes up from /db to project root
+cred_path = os.path.join(BASE_DIR, "config", "firebase_key.json")
 
+# Initialize Firebase
+cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
